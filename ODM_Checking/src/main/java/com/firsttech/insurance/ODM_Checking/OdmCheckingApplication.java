ff225202bc78b035d34c3622b41e1443bd3f18f8
PeterLiao <peter.liao@qianhe.com.tw>
2024-11-07 14:12:34 +0800

    new version

---
+++ ODM_Checking/src/main/java/com/firsttech/insurance/ODM_Checking/OdmCheckingApplication.java
@@ -0,0 +1,13 @@
+package com.firsttech.insurance.ODM_Checking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdmCheckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdmCheckingApplication.class, args);
+	}
+
+}
