ed51e4f77b8a7d8ba7649bc600fc5ee05a9c863e
PeterLiao <peter.liao@qianhe.com.tw>
2024-11-13 16:30:59 +0800

    add annotation enableSync

--- odm-checking/src/main/java/com/firsttech/insurance/odmchecking/OdmCheckingApplication.java
+++ odm-checking/src/main/java/com/firsttech/insurance/odmchecking/OdmCheckingApplication.java
@@ -3,9 +3,11 @@ package com.firsttech.insurance.odmchecking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
 @EnableScheduling
 public class OdmCheckingApplication extends SpringBootServletInitializer {
 
