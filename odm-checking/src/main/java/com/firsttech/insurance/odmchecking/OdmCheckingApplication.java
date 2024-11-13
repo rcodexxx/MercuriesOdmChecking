fe28cfdcd395480e7cdd027f68d1ae1ac510c2c6
PeterLiao <peter.liao@qianhe.com.tw>
2024-11-13 17:04:24 +0800

    fix enable tag

--- odm-checking/src/main/java/com/firsttech/insurance/odmchecking/OdmCheckingApplication.java
+++ odm-checking/src/main/java/com/firsttech/insurance/odmchecking/OdmCheckingApplication.java
@@ -7,8 +7,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
-@EnableScheduling
 public class OdmCheckingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
